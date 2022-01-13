cb6118428cc2da186a0d30159af6c0e81413a7ad
OnyekaIgwe <oncigwe@gmail.com>
2022-01-13 18:33:25 -0500

    Added Scriptbee

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,6 @@ export default function Home() {
           rel='icon'
           href='https://avatars.githubusercontent.com/u/75808489?v=4'
         />
-        <script async src='https://cdn.splitbee.io/sb.js'></script>
       </Head>
       <Flex>
         <Spacer />
